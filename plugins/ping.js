16d4ff6ec9fbd9f2211ffa58cbcecdf0dd49d0ae
skc-sketch112 <chunnachud@gmail.com>
2025-08-21 19:13:14 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "ping",
     description: "Replies with pong",
     execute: async (sock, msg, sender) => {
-        await sock.sendMessage(sender, { text: "pong ✅" });
+        await sock.sendMessage(sender, { text: "pong ✅ SOURAV_MD V4.08.09✨" });
     }
 };
