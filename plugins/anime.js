a138bb84abe89e45917c23a7f3e105cb92f7bccd
skc-sketch112 <chunnachud@gmail.com>
2025-08-22 23:41:13 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,12 +1,12 @@
 module.exports = {
     name: "anime",
     command: ["anime", "animes"],
-    description: "Send 5 random anime images",
+    description: "Send 5 random anime images safely",
     category: "Fun",
 
     async execute(sock, m, args) {
         try {
-            // Anime image links (add unlimited here)
+            // Anime image links (you can add unlimited here)
             const animeImages = [
                 "https://i.imgur.com/W1aD1zC.jpg",
                 "https://i.imgur.com/jt3A6Qp.jpg",
@@ -24,18 +24,19 @@ module.exports = {
             let selected = [];
             for (let i = 0; i < 5; i++) {
                 let randomImg = animeImages[Math.floor(Math.random() * animeImages.length)];
-                selected.push({ image: { url: randomImg } });
+                selected.push(randomImg);
             }
 
-            // Send as one album (single message)
-            await sock.sendMessage(
-                m.key.remoteJid,
-                { 
-                    forwarded: true, 
-                    messages: selected.map(img => ({ message: img })) 
-                },
-                { quoted: m }
-            );
+            // Send images one by one with delay
+            for (let img of selected) {
+                await sock.sendMessage(
+                    m.key.remoteJid,
+                    { image: { url: img }, caption: "🌸 Anime Pic" },
+                    { quoted: m }
+                );
+                // Safe delay (1s) so no spam/429 error
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            }
 
         } catch (err) {
             console.error("❌ Error in anime command:", err);
