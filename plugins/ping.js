a02e7b2951a532318a8ceb5b388291f6896c2da8
skc-sketch112 <chunnachud@gmail.com>
2025-08-23 15:09:30 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "ping",
     command: ["ping"],
     execute: async (sock, m, args) => {
-        await sock.sendMessage(m.key.remoteJid, { text: "🏓 Pong!SOURAV_MD V4.08.09✨✨⚡" }, { quoted: m });
+        await sock.sendMessage(m.key.remoteJid, { text: "🏓 Pong!SOURAV_MD V4.08.09⚡welcome to SOURAV_MD 🔥✨" }, { quoted: m });
     }
 };
