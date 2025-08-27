e2dd2ca07c90e483c736c9aa9c9bbc932fa7b76c
skc-sketch112 <chunnachud@gmail.com>
2025-08-27 14:51:12 +0530

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -7,23 +7,23 @@ module.exports = {
 
   async execute(sock, msg, args) {
     try {
-      const apiSub = args[0] || "";  // Optionally allow subreddit argument
-      const endpoint = apiSub
-        ? `https://memesapi.vercel.app/give/${apiSub}`
-        : `https://memesapi.vercel.app/give`;
+      const subReddits = ["memes", "dankmemes", "wholesomememes", "me_irl", "funny"];
+      const randomSub = subReddits[Math.floor(Math.random() * subReddits.length)];
+      const url = `https://www.reddit.com/r/${randomSub}/top.json?limit=50&t=day`;
 
-      const res = await fetch(endpoint);
-      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      const res = await fetch(url, { headers: { "User-Agent": "WhatsAppBot/1.0" } });
       const json = await res.json();
 
-      const memeData = json.memes && json.memes[0];
-      if (!memeData || !memeData.url) {
-        throw new Error("No meme found");
-      }
+      const posts = json.data.children.filter(p => !p.data.over_18 && p.data.post_hint === "image");
+      if (!posts.length) throw new Error("No memes found");
+
+      const randomPost = posts[Math.floor(Math.random() * posts.length)];
+      const memeUrl = randomPost.data.url;
+      const title = randomPost.data.title;
 
       await sock.sendMessage(msg.key.remoteJid, {
-        image: { url: memeData.url },
-        caption: `🤣 *${memeData.title}*\n\n🌍 from r/${memeData.subreddit}`
+        image: { url: memeUrl },
+        caption: `🤣 *${title}*\n\n🌍 from r/${randomSub}`
       }, { quoted: msg });
 
     } catch (e) {
